ca7b792aac39dbb2113dd55de604e873ed802ee3
SeiTbNao - Djunio <djunio_rmf@hotmail.com>
2021-02-16 00:21:40 -0300

    root

---
+++ Launcher v2/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace WYDLauncher
+{
+    partial class Window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Window));
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.strtGameBtn = new System.Windows.Forms.Button();
+            this.closeBtn = new System.Windows.Forms.Button();
+            this.minimizeBtn = new System.Windows.Forms.Button();
+            this.patchNotes = new System.Windows.Forms.WebBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new WYDLauncher.TextProgressBar();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // strtGameBtn
+            // 
+            this.strtGameBtn.BackColor = System.Drawing.Color.LightGray;
+            this.strtGameBtn.FlatAppearance.BorderSize = 2;
+            this.strtGameBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.strtGameBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.strtGameBtn.ForeColor = System.Drawing.Color.DimGray;
+            this.strtGameBtn.Location = new System.Drawing.Point(738, 386);
+            this.strtGameBtn.Name = "strtGameBtn";
+            this.strtGameBtn.Size = new System.Drawing.Size(150, 52);
+            this.strtGameBtn.TabIndex = 1;
+            this.strtGameBtn.Text = "JOGAR";
+            this.strtGameBtn.UseVisualStyleBackColor = false;
+            this.strtGameBtn.Click += new System.EventHandler(this.strtGameBtn_Click);
+            // 
+            // closeBtn
+            // 
+            this.closeBtn.BackColor = System.Drawing.Color.Transparent;
+            this.closeBtn.FlatAppearance.BorderSize = 0;
+            this.closeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeBtn.Location = new System.Drawing.Point(866, 0);
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.Size = new System.Drawing.Size(34, 26);
+            this.closeBtn.TabIndex = 3;
+            this.closeBtn.UseVisualStyleBackColor = false;
+            this.closeBtn.Click += new System.EventHandler(this.closeBtn_Click);
+            this.closeBtn.MouseEnter += new System.EventHandler(this.closeBtn_MouseEnter);
+            this.closeBtn.MouseLeave += new System.EventHandler(this.closeBtn_MouseLeave);
+            // 
+            // minimizeBtn
+            // 
+            this.minimizeBtn.BackColor = System.Drawing.Color.Transparent;
+            this.minimizeBtn.FlatAppearance.BorderSize = 0;
+            this.minimizeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.minimizeBtn.Location = new System.Drawing.Point(832, 0);
+            this.minimizeBtn.Name = "minimizeBtn";
+            this.minimizeBtn.Size = new System.Drawing.Size(34, 26);
+            this.minimizeBtn.TabIndex = 4;
+            this.minimizeBtn.UseVisualStyleBackColor = false;
+            this.minimizeBtn.Click += new System.EventHandler(this.minimizeBtn_Click);
+            this.minimizeBtn.MouseEnter += new System.EventHandler(this.minimizeBtn_MouseEnter);
+            this.minimizeBtn.MouseLeave += new System.EventHandler(this.minimizeBtn_MouseLeave);
+            // 
+            // patchNotes
+            // 
+            this.patchNotes.Location = new System.Drawing.Point(-2, 26);
+            this.patchNotes.MinimumSize = new System.Drawing.Size(20, 20);
+            this.patchNotes.Name = "patchNotes";
+            this.patchNotes.ScrollBarsEnabled = false;
+            this.patchNotes.Size = new System.Drawing.Size(902, 426);
+            this.patchNotes.TabIndex = 5;
+            this.patchNotes.Url = new System.Uri("", System.UriKind.Relative);
+            this.patchNotes.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.patchNotes_DocumentCompleted);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightGray;
+            this.button1.FlatAppearance.BorderSize = 2;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.DimGray;
+            this.button1.Location = new System.Drawing.Point(698, 386);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(34, 29);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "✛";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.progressBar1.CustomText = "";
+            this.progressBar1.ForeColor = System.Drawing.Color.DarkRed;
+            this.progressBar1.Location = new System.Drawing.Point(12, 421);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.ProgressColor = System.Drawing.Color.LightGreen;
+            this.progressBar1.Size = new System.Drawing.Size(720, 17);
+            this.progressBar1.TabIndex = 0;
+            this.progressBar1.TextColor = System.Drawing.Color.Black;
+            this.progressBar1.TextFont = new System.Drawing.Font("Times New Roman", 11F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.progressBar1.VisualMode = WYDLauncher.ProgressBarDisplayMode.Percentage;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(900, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.minimizeBtn);
+            this.Controls.Add(this.closeBtn);
+            this.Controls.Add(this.strtGameBtn);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.patchNotes);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Window";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WYD Launcher";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public WYDLauncher.TextProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button strtGameBtn;
+        private System.Windows.Forms.Button closeBtn;
+        private System.Windows.Forms.Button minimizeBtn;
+        private System.Windows.Forms.WebBrowser patchNotes;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Launcher v2/Form1.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Ionic.Zip;
+
+namespace WYDLauncher
+{
+    public partial class Window : Form
+    {
+        public List<string> lista = new List<string>();
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        public int zipCount = 0;
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+        public string Server =  "";
+        private int CompletserverVersion = 0;
+        private int downloadlastupdate = 0;
+        private XDocument       serverXml;
+        public Window()
+        {
+            InitializeComponent();
+            readSettings();
+            backgroundWorker1.RunWorkerAsync();
+            strtGameBtn.Enabled = false;
+            progressBar1.ProgressColor = Color.Gray;
+            progressBar1.Value = 0;
+        }
+
+        public void readSettings()
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines("./settings.txt");
+                foreach (string line in lines)
+                {
+                    if (!line.Contains("http"))
+                        continue;
+
+                    Server = line;
+                }
+
+                try
+                {
+                    serverXml = XDocument.Load(@Server + "Updates.xml");
+                    patchNotes.Url = new Uri(serverXml.Root.Element("config").Element("notic").Value);
+                    CompletserverVersion = int.Parse(serverXml.Root.Element("config").Element("currentversion").Value);
+                    downloadlastupdate = int.Parse(serverXml.Root.Element("config").Element("downloadlastupdate").Value);
+                }
+                catch (Exception Ex)
+                {
+                    MessageBox.Show("Servidor de atualização falhou");
+                    this.Close();
+                }
+            }
+            catch(Exception Ex)
+            {
+                MessageBox.Show("Não foi possível localizar o arquivo settings.txt");
+                this.Close();
+            }
+           
+        }
+        private void Form1_MouseDown(object sender,
+        System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void closeBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void closeBtn_MouseEnter(object sender, EventArgs e)
+        {
+            closeBtn.BackgroundImage = Properties.Resources.close2;
+        }
+
+        private void closeBtn_MouseLeave(object sender, EventArgs e)
+        {
+            closeBtn.BackgroundImage = Properties.Resources.close1;
+        }
+
+        private void minimizeBtn_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void minimizeBtn_MouseEnter(object sender, EventArgs e)
+        {
+            minimizeBtn.BackgroundImage = Properties.Resources.minimize2;
+        }
+
+        private void minimizeBtn_MouseLeave(object sender, EventArgs e)
+        {
+            minimizeBtn.BackgroundImage = Properties.Resources.minimize1;
+        }
+
+        static bool deleteFile(string f)
+        {
+            try
+            {
+                File.Delete(f);
+                return true;
+            }
+            catch (IOException)
+            {
+                return false;
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            lista.Clear();
+
+            string Root = AppDomain.CurrentDomain.BaseDirectory;
+
+            FileStream fs = null;
+            if (!File.Exists("version"))
+            {
+                using (fs = File.Create("version"))
+                {
+
+                }
+
+                using (StreamWriter sw = new StreamWriter("version"))
+                {
+                    sw.Write("7559");
+                }
+            }
+
+            string lclVersion;
+            using (StreamReader reader = new StreamReader("version"))
+            {
+                lclVersion = reader.ReadLine();
+            }
+            decimal localVersion = decimal.Parse(lclVersion);
+
+ 
+
+            foreach (XElement update in serverXml.Descendants("update"))
+            {
+                string version = update.Element("version").Value;
+                string file = update.Element("file").Value;
+
+                decimal serverVersion = decimal.Parse(version);
+
+
+                string sUrlToReadFileFrom = Server + file;
+
+                string sFilePathToWriteFileTo = Root + file;
+
+                bool normalCondition = serverVersion > localVersion;
+                if (downloadlastupdate == 1)
+                    normalCondition = serverVersion >= localVersion;
+
+                if (normalCondition)
+                {
+                    Uri url = new Uri(sUrlToReadFileFrom);
+                    HttpWebRequest request = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(url);
+                    HttpWebResponse response = (System.Net.HttpWebResponse)request.GetResponse();
+                    response.Close();
+                    Int64 iSize = response.ContentLength;
+                    Int64 iRunningByteTotal = 0;
+
+                    using (WebClient client = new WebClient())
+                    {
+                        using (Stream streamRemote = client.OpenRead(new Uri(sUrlToReadFileFrom)))
+                        {
+                            using (Stream streamLocal = new FileStream(sFilePathToWriteFileTo, FileMode.Create, FileAccess.Write, FileShare.None))
+                            {
+                                int iByteSize = 0;
+                                byte[] byteBuffer = new byte[iSize];
+                                while ((iByteSize = streamRemote.Read(byteBuffer, 0, byteBuffer.Length)) > 0)
+                                {
+                                    streamLocal.Write(byteBuffer, 0, iByteSize);
+                                    iRunningByteTotal += iByteSize;
+
+                                    double dIndex = (double)(iRunningByteTotal);
+                                    double dTotal = (double)byteBuffer.Length;
+                                    double dProgressPercentage = (dIndex / dTotal);
+                                    int iProgressPercentage = (int)(dProgressPercentage * 100);
+
+                                    backgroundWorker1.ReportProgress(iProgressPercentage);
+                                }
+
+                                streamLocal.Close();
+
+                            }
+
+                            streamRemote.Close();
+                        }
+                    }
+
+                    lista.Add(file);
+                    using (ZipFile zip = ZipFile.Read(file))
+                    {
+                        progressBar1.Maximum = zip.Count;
+                        foreach (ZipEntry zipFiles in zip)
+                        {
+                            progressBar1.CustomText = "Extraindo arquivos... " + zipFiles.FileName;
+                            progressBar1.VisualMode = ProgressBarDisplayMode.TextAndCurrProgress;
+                            zipFiles.Extract(Root + "", true);
+                            progressBar1.Value++;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            
+            // downloadLbl.ForeColor = System.Drawing.Color.Silver;
+            strtGameBtn.Enabled = false;
+            progressBar1.VisualMode = ProgressBarDisplayMode.TextAndPercentage;
+            progressBar1.CustomText = "Baixando Atualizações";
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            strtGameBtn.Enabled = true;
+            strtGameBtn.Enabled = true;
+            //progressBar1.Value = 100;
+            // this.downloadLbl.ForeColor = System.Drawing.Color.FromArgb(0, 121, 203);
+            progressBar1.VisualMode = ProgressBarDisplayMode.CustomText;
+
+            lista.ForEach(i => deleteFile(i));
+         
+            //download new version file
+            WebClient webClient = new WebClient();
+
+
+            FileStream fs = null;
+            if (!File.Exists("version"))
+            {
+                using (fs = File.Create("version"))
+                {
+
+                }
+
+                using (StreamWriter sw = new StreamWriter("version"))
+                {
+                    sw.Write("7559");
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = new StreamWriter("version"))
+                {
+                    sw.Write(CompletserverVersion);
+                }
+            }
+
+            progressBar1.CustomText = "Client Atualizado !";
+        }
+
+
+        //Starts the game
+        private void strtGameBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start("WYD.exe", "\\starting by launcher");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("WYD.exe não foi encontrado");
+            }
+            this.Close();
+        }
+
+        private void patchNotes_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start("config.exe", "\\no");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("config.exe não foi encontrado");
+            }
+       
+        }
+    }
+}
